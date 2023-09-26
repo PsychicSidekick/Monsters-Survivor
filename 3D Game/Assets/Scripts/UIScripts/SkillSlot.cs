c6773e5525a4a89a5dad1b3966468eccfe67a348
PsychicSidekick <65291503+PsychicSidekick@users.noreply.github.com>
2023-09-26 14:54:16 +0800

    Add text to skill buttons for showing cooldown times

--- 3D Game/Assets/Scripts/UIScripts/SkillSlot.cs
+++ 3D Game/Assets/Scripts/UIScripts/SkillSlot.cs
@@ -1,13 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SkillSlot : MonoBehaviour
 {
     public Skill chosenSkill;
     public int skillSlotID;
+    public SkillHolder skillHolder;
 
     public GameObject chooseSkillPanel;
+    public TMP_Text cooldownText;
+
+    private Button button;
+    private Image image;
+
+    private void Start()
+    {
+        skillHolder = PlayerControl.instance.GetComponent<SkillHandler>().skills[skillSlotID];
+        button = GetComponent<Button>();
+        image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (chosenSkill != null)
+        {
+            if (skillHolder.cooldownTime > 0)
+            {
+                button.enabled = false;
+                image.color = button.colors.disabledColor;
+                cooldownText.text = Mathf.Ceil(skillHolder.cooldownTime).ToString();
+            }
+            else
+            {
+                button.enabled = true;
+                image.color = button.colors.normalColor;
+                cooldownText.text = "";
+            }
+        }
+    }
 
     public void OnClick()
     {
@@ -32,6 +65,6 @@ public class SkillSlot : MonoBehaviour
 
     public void SetPlayerSkill()
     {
-        PlayerControl.instance.GetComponent<SkillHandler>().skills[skillSlotID].skill = chosenSkill;
+        skillHolder.skill = chosenSkill;
     }
 }
